f6f7c5e420b1c833c4152c7290dc5e34eac17e61
Simhateja17 <mg8751721@gmail.com>
2025-04-16 22:56:06 +0530

    Update seller product page v2

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,31 @@ const nextConfig = {
   images: {
     domains: ["ljotxxopiuikiwnbhnhn.supabase.co"],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+        ],
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://fastandfab.in/api/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
