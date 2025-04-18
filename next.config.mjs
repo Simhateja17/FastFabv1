c4e9cff57273c76cffff8c393b9f68b4f09a8a67
Simhateja17 <mg8751721@gmail.com>
2025-04-18 15:00:28 +0530

    Seller Prodducts Page

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,6 @@ const nextConfig = {
   images: {
     domains: ["ljotxxopiuikiwnbhnhn.supabase.co"],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_URL 
-          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*` 
-          : 'http://localhost:8000/api/:path*',
-      },
-    ]
-  },
 };
 
 export default nextConfig;
